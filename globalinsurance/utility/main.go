8310acb01e1039ea18ef152f13316c6f92062bc2
eswaribala <parameswaribala@gmail.com>
2021-06-28 10:50:47 +0530

    Added mysql lib

--- globalinsurance/utility/main.go
+++ globalinsurance/utility/main.go
@@ -1,13 +1,25 @@
 package main
 
-func main(){
-/*
-	vehicle:=models.Vehicle{
-		"TN-02-483568","Honda",
-		&models.Date{12,12,2020},"A23452","K42545","Diesel",
-		"Blue",
+import (
+	"fmt"
+	"github.com/goadvanced/globalinsurance/stores"
+)
+
+func main() {
+	/*
+	   	vehicle:=models.Vehicle{
+	   		"TN-02-483568","Honda",
+	   		&models.Date{12,12,2020},"A23452","K42545","Diesel",
+	   		"Blue",
+	   	}
+
+	     fmt.Printf("The Vehicle Details are %+v", vehicle)
+	*/
+
+	db := stores.ConnectionHelper()
+
+	if db != nil {
+		fmt.Println("Connected")
 	}
 
-  fmt.Printf("The Vehicle Details are %+v", vehicle)
-*/
 }
