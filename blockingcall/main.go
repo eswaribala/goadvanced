343c5ada005a84be3e756f48f014c5a02b6df82d
eswaribala <parameswaribala@gmail.com>
2021-06-28 14:25:40 +0530

    Added mysql lib

--- blockingcall/main.go
+++ blockingcall/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
@@ -29,14 +31,24 @@ func main() {
 }
 
 func ReadLinks(link string, c chan string) {
-	_, err := http.Get(link)
+	response, err := http.Get(link)
 	if err != nil {
 		fmt.Println("The site is down", link)
 		//write the message to channel
 		c <- "The site is down"
 	} else {
 		fmt.Println("The site is up", link)
+		go ReadUrl(response)
 		//write message to channel
 		c <- "The site is up"
 	}
+}
+
+func ReadUrl(response *http.Response) {
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal("Could not read contents", err)
+	}
+	fmt.Printf("The Content length %d", len(body))
 }
