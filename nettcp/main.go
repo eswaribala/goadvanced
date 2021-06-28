aea1f10d1ba2ca17df2339073a4f85b00fd217a4
eswaribala <parameswaribala@gmail.com>
2021-06-28 16:44:56 +0530

    Added mysql lib

---
+++ nettcp/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		fmt.Println("Connection could not be created")
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go WriteData(conn) // handle one connection at a time
+	}
+}
+
+func WriteData(conn net.Conn) {
+	defer conn.Close()
+	for {
+		_, err := io.WriteString(conn, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
