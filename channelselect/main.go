d8180830abc44aeb4a63ca5a0328361b32923f13
eswaribala <parameswaribala@gmail.com>
2021-06-28 15:55:14 +0530

    Added mysql lib

--- channelselect/main.go
+++ channelselect/main.go
@@ -1,20 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 func main() {
+	//created channel
 	orderChannel := make(chan string)
 	paymentChannel := make(chan string)
 	locationChannel := make(chan string)
 	//send data
 	go func() {
-		orderChannel <- "Order Placed"
+		for {
+			time.Sleep(5 * time.Second)
+			orderChannel <- "Order Placed" + "," + strconv.Itoa(int(rand.Int31n(1000)))
+		}
 	}()
 	go func() {
-		paymentChannel <- "Payment Success"
+		for {
+			time.Sleep(5 * time.Second)
+			paymentChannel <- "Payment Success" + "," + strconv.Itoa(int(rand.Int31n(1000)))
+		}
 	}()
 	go func() {
-		locationChannel <- "Location Found"
+		for {
+			time.Sleep(5 * time.Second)
+			locationChannel <- "Location Found" + "," + strconv.Itoa(int(rand.Int31n(1000)))
+		}
 	}()
 
 	//receiver go routine
@@ -31,4 +46,6 @@ func main() {
 		}
 	}()
 
+	var input string
+	fmt.Scanln(&input)
 }
